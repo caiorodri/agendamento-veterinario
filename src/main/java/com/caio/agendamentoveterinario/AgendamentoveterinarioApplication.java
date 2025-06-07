9ac9b94dc3417978ed0b6a5af532717518625d27
caiorodri <caiodossantosrodrigues@gmail.com>
2025-06-07 14:19:51 -0300

    Criação dos arquivos padrões

---
+++ src/main/java/com/caio/agendamentoveterinario/AgendamentoveterinarioApplication.java
@@ -0,0 +1,13 @@
+package com.caio.agendamentoveterinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoveterinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoveterinarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/agendamentoveterinario/AgendamentoveterinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.agendamentoveterinario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoveterinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
