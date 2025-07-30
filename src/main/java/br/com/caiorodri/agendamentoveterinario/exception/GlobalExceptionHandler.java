ed65e9a317d826b858bf328cf18dd7631051b3f9
caiorodri <caiodossantosrodrigues@gmail.com>
2025-07-29 22:53:29 -0300

    feat(exception): criar handler global de exceções

---
+++ src/main/java/br/com/caiorodri/agendamentoveterinario/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package br.com.caiorodri.agendamentoveterinario.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        
+    	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    
+    }
+	
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+     
+    	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                             .body("Erro interno no servidor: " + ex.getMessage());
+    
+    }
+    
+}
