68c86998d9fc7d5487f46e9c6e1eff7b6c825ca1
caiorodri <caiodossantosrodrigues@yahoo.com.br>
2025-12-04 13:10:33 -0300

    fix(scheduler): adicionar anotação para a scheduler funcionar

--- src/main/java/br/com/caiorodri/agendamentoveterinario/scheduler/AgendamentoStatusScheduler.java
+++ src/main/java/br/com/caiorodri/agendamentoveterinario/scheduler/AgendamentoStatusScheduler.java
@@ -3,6 +3,7 @@ package br.com.caiorodri.agendamentoveterinario.scheduler;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,8 @@ public class AgendamentoStatusScheduler {
     private static final Integer ID_STATUS_ABERTO = 1;
     private static final Integer ID_STATUS_PERDIDO = 4;
 
-    @Scheduled(cron = "0 0/30 * * * *")
+    @Scheduled(cron = "0 0/30 * * * *", zone = "America/Sao_Paulo")
+    @Transactional
     public void marcarAgendamentosComoPerdido() {
 
         logger.info("Iniciando verificação de agendamentos expirados...");
